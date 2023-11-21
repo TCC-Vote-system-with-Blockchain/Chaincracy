f7b27bd5d54421d9f35164d37d03d4456ff9dcef
Eduardo Campos Gonçalves <edu.ggoncalves37@gmail.com>
2023-11-21 01:11:41 -0300

    fix(getPicture): removed some pictures

--- frontend/src/utils/getPicture.ts
+++ frontend/src/utils/getPicture.ts
@@ -1,14 +1,14 @@
 import pic1 from '../../public/assets/picture1.jpg'
-import pic2 from '../../public/assets/picture2.jpg'
-import pic3 from '../../public/assets/picture3.jpg'
 import pic4 from '../../public/assets/picture4.jpg'
+import pic5 from '../../public/assets/picture5.jpg'
+import pic6 from '../../public/assets/picture6.jpg'
 
 export const getPicture = () => {
     const pictures = [
         pic1,
-        pic2,
-        pic3,
-        pic4
+        pic4,
+        pic5,
+        pic6
     ]
     var index = Math.floor(Math.random() * 3) + 1;
     return pictures[index];
